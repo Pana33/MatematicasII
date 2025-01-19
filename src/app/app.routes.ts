5be459da208472ba15f031393d9bb9312072321b
Pana33 <62369981+Pana33@users.noreply.github.com>
2025-01-19 01:21:21 -0600

    Update app.routes.ts
    Se agregan las primeras rutas correspondientes al home y la primera leccion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { PageRoutes } from './shared/enums/page-routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',redirectTo:PageRoutes.HOME,pathMatch:'full'},
+    {path:PageRoutes.HOME,loadComponent: () => import("./pages/home/home.component").then(m => m.HomeComponent),children:[
+        {path:'',redirectTo:PageRoutes.HOME,pathMatch:'full'},
+        {path:PageRoutes.APPROXIMATE_AREA_UNDER_THE_CURVE,loadComponent: () => import("./pages/approximate-area-under-the-curve/approximate-area-under-the-curve.component").then(m => m.ApproximateAreaUnderTheCurveComponent)},
+        {path:'**',redirectTo:PageRoutes.APPROXIMATE_AREA_UNDER_THE_CURVE,pathMatch:'full'}
+    ]},
+    {path:'**',redirectTo:PageRoutes.HOME,pathMatch:'full'}
+];
