f4a3614f890b0f399e453afc554c684452c55206
Pana33 <62369981+Pana33@users.noreply.github.com>
2025-01-19 02:26:51 -0600

    Style navbar
    Se le da estilo al navbar para mostrar el logo del cugdl y el menu que se piensa tener disponible

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { PageRoutes } from '../../shared/enums/page-routes';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  readonly pageRoutes = PageRoutes
 }
