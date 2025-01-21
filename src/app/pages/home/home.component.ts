ed35a0301f08d8bd07e545fab5d34131b15c8d47
Pana33 <62369981+Pana33@users.noreply.github.com>
2025-01-21 14:26:45 -0600

    Style ortography
    Se corrigen errores de ortografia

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { PageRoutes } from '../../shared/enums/page-routes';
+import { ModalPersonInfoComponent } from "./modal-person-info/modal-person-info.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, ModalPersonInfoComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
