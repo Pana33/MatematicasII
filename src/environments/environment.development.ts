10973c384d6bee9d58a2197b1d3cb4738bdc2333
Pana33 <62369981+Pana33@users.noreply.github.com>
2025-01-21 22:46:34 -0600

    Create environment files
    Se crean los archivos para las variables de entorno y se dejan configuradas con los accesos a firebase

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebaseConfig:{
+        apiKey: "AIzaSyB8A33oXUG2K6UTzvf_ELgdw9yJ5I_BlQo",
+        authDomain: "cugdl-projects.firebaseapp.com",
+        projectId: "cugdl-projects",
+        storageBucket: "cugdl-projects.firebasestorage.app",
+        messagingSenderId: "39286389381",
+        appId: "1:39286389381:web:694e1e38da65eec931c7ec",
+        measurementId: "G-0XB7BNWPDK"
+    }
+};
