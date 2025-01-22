1e82df190e8dc3468345583a8d05fb2052338a05
Pana33 <62369981+Pana33@users.noreply.github.com>
2025-01-21 23:01:52 -0600

    Install angular fire
    Se agrega angular fire para el uso de analytics y de paso se configura para realizar el despliegue al servidor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,14 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAnalytics, provideAnalytics, ScreenTrackingService } from '@angular/fire/analytics';
+import { environment } from '../environments/environment.development';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes), provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAnalytics(() => getAnalytics()),
+    ScreenTrackingService
+  ]
 };
