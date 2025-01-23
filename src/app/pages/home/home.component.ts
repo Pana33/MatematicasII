6ed411cc4d436653125b3069e8fbb4ecc08823c3
Pana33 <62369981+Pana33@users.noreply.github.com>
2025-01-22 22:20:35 -0600

    Feat update active route
    Se realiza la actualizacion de la ruta activa para que el usuario identifique que es lo que esta viendo

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { PageRoutes } from '../../shared/enums/page-routes';
 import { ModalPersonInfoComponent } from "./modal-person-info/modal-person-info.component";
+import { Subscription } from 'rxjs';
+import { ActiveRouteService } from '../../services/active-route/active-route.service';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, ModalPersonInfoComponent],
+  imports: [RouterOutlet, RouterLink, ModalPersonInfoComponent, NgClass, RouterLinkActive],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+  activeRouteService = inject(ActiveRouteService)
   readonly pageRoutes = PageRoutes
+  activeRoute$!:Subscription
+  activeRoute:number = 0
+
+  ngOnInit(): void {
+    this.activeRoute$ = this.activeRouteService.routeToActive.subscribe(lastRoute => this.activeRoute = lastRoute)
+  }
+
+  ngOnDestroy(): void {
+    this.activeRoute$?.unsubscribe()
+  }
+
 }
