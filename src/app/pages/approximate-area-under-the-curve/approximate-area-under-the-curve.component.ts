83617ea035ee9abcd1946973779abbb717943780
Pana33 <62369981+Pana33@users.noreply.github.com>
2025-01-22 22:40:17 -0600

    Feat display operations
    Se muestran las operaciones que se realizan para llegar al resultado de la aproximacion del area bajo la curva

--- src/app/pages/approximate-area-under-the-curve/approximate-area-under-the-curve.component.ts
+++ src/app/pages/approximate-area-under-the-curve/approximate-area-under-the-curve.component.ts
@@ -1,15 +1,18 @@
 import { Component, inject } from '@angular/core';
 import { PersonInfoService } from '../../services/person-info/person-info.service';
+import { OperationsComponent } from "./operations/operations.component";
 
 @Component({
   selector: 'app-approximate-area-under-the-curve',
   standalone: true,
-  imports: [],
+  imports: [OperationsComponent],
   templateUrl: './approximate-area-under-the-curve.component.html',
   styleUrl: './approximate-area-under-the-curve.component.scss'
 })
 export class ApproximateAreaUnderTheCurveComponent {
   private personService = inject(PersonInfoService)
+  stepClicked:number = 0
+
   emitPerson(idToFind:string){
     this.personService.emitPerson(idToFind)
   }
