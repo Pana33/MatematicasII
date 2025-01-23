4d8c75fb57d887761d108100e076647f4d667e3d
Pana33 <62369981+Pana33@users.noreply.github.com>
2025-01-22 22:19:13 -0600

    Create active route guard
    Se crea un guard para enviar que ruta es la que se esta activando y asi marcar en la navbar en que ruta se encuentra el usuario

---
+++ src/app/shared/guards/active-route/active-route.guard.ts
@@ -0,0 +1,15 @@
+import { inject } from '@angular/core';
+import { CanActivateFn } from '@angular/router';
+import { ActiveRouteService } from '../../../services/active-route/active-route.service';
+import { PageRoutes } from '../../enums/page-routes';
+
+export const activeRouteGuard: CanActivateFn = (route, state) => {
+  let activeRouteService = inject(ActiveRouteService)
+  let routeToActivate = 0
+  switch(route.routeConfig?.path){
+    case PageRoutes.APPROXIMATE_AREA_UNDER_THE_CURVE:
+      routeToActivate = 1
+  }
+  activeRouteService.changeRoute(routeToActivate)
+  return true;
+};
