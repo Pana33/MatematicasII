9671ccab66dc15bb6a67fc0ec94ad9645f09295b
Pana33 <62369981+Pana33@users.noreply.github.com>
2025-01-22 22:21:12 -0600

    Update app.routes.ts
    Se agrega el guard que con ayuda del servicio estara informando al componente home que tiene la barra de navegacion, en donde se encuentra el usuario para asi estar indicando que es lo que se esta viendo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { Routes } from '@angular/router';
 import { PageRoutes } from './shared/enums/page-routes';
+import { activeRouteGuard } from './shared/guards/active-route/active-route.guard';
 
 export const routes: Routes = [
     {path:'',redirectTo:PageRoutes.HOME,pathMatch:'full'},
     {path:PageRoutes.HOME,loadComponent: () => import("./pages/home/home.component").then(m => m.HomeComponent),children:[
         {path:'',redirectTo:PageRoutes.HOME,pathMatch:'full'},
-        {path:PageRoutes.BOOK_COVER,loadComponent: () => import("./pages/book-cover/book-cover.component").then(m => m.BookCoverComponent)},
-        {path:PageRoutes.APPROXIMATE_AREA_UNDER_THE_CURVE,loadComponent: () => import("./pages/approximate-area-under-the-curve/approximate-area-under-the-curve.component").then(m => m.ApproximateAreaUnderTheCurveComponent)},
+        {path:PageRoutes.BOOK_COVER,loadComponent: () => import("./pages/book-cover/book-cover.component").then(m => m.BookCoverComponent),canActivate:[activeRouteGuard]},
+        {path:PageRoutes.APPROXIMATE_AREA_UNDER_THE_CURVE,loadComponent: () => import("./pages/approximate-area-under-the-curve/approximate-area-under-the-curve.component").then(m => m.ApproximateAreaUnderTheCurveComponent),canActivate:[activeRouteGuard]},
         {path:'**',redirectTo:PageRoutes.BOOK_COVER,pathMatch:'full'}
     ]},
     {path:'**',redirectTo:PageRoutes.HOME,pathMatch:'full'}
