024e89149e4aa6c8805035d2db8061ad5dcdd4ff
Pana33 <62369981+Pana33@users.noreply.github.com>
2025-02-17 22:52:32 -0600

    Feat navigation to integration by parts
    Se agrega la navegacion para la pagina de integracion por partes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,17 @@
 import { Routes } from '@angular/router';
 import { PageRoutes } from './shared/enums/page-routes';
-import { activeRouteGuard } from './shared/guards/active-route/active-route.guard';
+import { activeRouteForUnitOneGuard } from './shared/guards/active-route-for-unit-one/active-route-for-unit-one.guard';
+import { disableActiveRouteForUnitsGuard } from './shared/guards/disable-active-route-for-units/disable-active-route-for-units.guard';
 
 export const routes: Routes = [
     {path:'',redirectTo:PageRoutes.HOME,pathMatch:'full'},
     {path:PageRoutes.HOME,loadComponent: () => import("./pages/home/home.component").then(m => m.HomeComponent),children:[
         {path:'',redirectTo:PageRoutes.HOME,pathMatch:'full'},
-        {path:PageRoutes.BOOK_COVER,loadComponent: () => import("./pages/book-cover/book-cover.component").then(m => m.BookCoverComponent),canActivate:[activeRouteGuard]},
-        {path:PageRoutes.APPROXIMATE_AREA_UNDER_THE_CURVE,loadComponent: () => import("./pages/approximate-area-under-the-curve/approximate-area-under-the-curve.component").then(m => m.ApproximateAreaUnderTheCurveComponent),canActivate:[activeRouteGuard]},
-        {path:PageRoutes.DEFINITE_AND_INDEFINITE_INEGRAL,loadComponent: () => import("./pages/definite-and-indefinite-integral/definite-and-indefinite-integral.component").then(m => m.DefiniteAndIndefiniteIntegralComponent),canActivate:[activeRouteGuard]},
-        {path:PageRoutes.INEGRAL_BY_SUSTITUTION,loadComponent: () => import("./pages/integral-by-sustitution/integral-by-sustitution.component").then(m => m.IntegralBySustitutionComponent),canActivate:[activeRouteGuard]},
+        {path:PageRoutes.BOOK_COVER,loadComponent: () => import("./pages/book-cover/book-cover.component").then(m => m.BookCoverComponent),canActivate:[disableActiveRouteForUnitsGuard]},
+        {path:PageRoutes.APPROXIMATE_AREA_UNDER_THE_CURVE,loadComponent: () => import("./pages/approximate-area-under-the-curve/approximate-area-under-the-curve.component").then(m => m.ApproximateAreaUnderTheCurveComponent),canActivate:[activeRouteForUnitOneGuard]},
+        {path:PageRoutes.DEFINITE_AND_INDEFINITE_INEGRAL,loadComponent: () => import("./pages/definite-and-indefinite-integral/definite-and-indefinite-integral.component").then(m => m.DefiniteAndIndefiniteIntegralComponent),canActivate:[activeRouteForUnitOneGuard]},
+        {path:PageRoutes.INEGRAL_BY_SUSTITUTION,loadComponent: () => import("./pages/integral-by-sustitution/integral-by-sustitution.component").then(m => m.IntegralBySustitutionComponent),canActivate:[activeRouteForUnitOneGuard]},
+        {path:PageRoutes.INTEGRATION_BY_PARTS,loadComponent: () => import("./pages/integration-by-parts/integration-by-parts.component").then(m => m.IntegrationByPartsComponent),canActivate:[activeRouteForUnitOneGuard]},
         {path:'**',redirectTo:PageRoutes.BOOK_COVER,pathMatch:'full'}
     ]},
     {path:'**',redirectTo:PageRoutes.HOME,pathMatch:'full'}
--- src/app/shared/enums/page-routes.ts
+++ src/app/shared/enums/page-routes.ts
@@ -4,4 +4,5 @@ export enum PageRoutes {
     APPROXIMATE_AREA_UNDER_THE_CURVE = 'approximate-area-under-the-curve',
     DEFINITE_AND_INDEFINITE_INEGRAL = 'definite-and-indefinite-integral',
     INEGRAL_BY_SUSTITUTION = 'integral-by-sustitution',
+    INTEGRATION_BY_PARTS = 'integration-by-parts',
 }
